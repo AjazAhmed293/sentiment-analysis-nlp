21936f2832ed74db6b91cdb58b5521a31cb7af87
AjazAhmed293 <Ajaz67587@gmail.com>
2025-07-28 23:37:00 +0530

    Initial commit with dataset and project description

---
+++ project.py
@@ -0,0 +1,3 @@
+import pandas as pd
+dataset=pd.read_csv(r"C:\Users\ajaz6\OneDrive\Desktop\sentiment_analysis_nlp\data\imdb.csv\IMDB Dataset.csv")
+print(dataset.head())  # Display the first few rows of the dataset
